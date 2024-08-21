d9d1b998452119a0f3802ed623a4efb26a54f479
贾一饼 <lby.i@qq.com>
2024-08-21 18:37:22 +0800

    update

--- src/time/time.js
+++ src/time/time.js
@@ -1,3 +1,4 @@
-const dateString = '2024-01-31 17:07:39';
-const timestamp = new Date(dateString).getTime();
-console.log(timestamp);
+
+function timestamp(dateString) {
+ return new Date(dateString).getTime()
+}
