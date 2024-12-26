b7a1e0fda950bf93298bcc1c27b2f589e8d95035
贾一饼 <lby.i@qq.com>
2024-12-26 22:04:16 +0800

    fix: arrayToMap

--- src/compatible/array.ts
+++ src/compatible/array.ts
@@ -1,13 +1,13 @@
-export function arr2map(arr: any[]): Map<any, any> {
+export function arrayToMap<T, K>(arr: any[], getKey: (T) => K): Map<K, T[]> {
     return arr.reduce((accumulator, currentValue) => {
         // 获取当前元素的 key 和 value
-
+        const key = getKey(currentValue)
         // 如果 accumulator 中已经有这个 key，则在其对应的数组中添加 value
-        if (accumulator.has(currentValue.key)) {
-            accumulator.get(currentValue.key).push(currentValue);
+        if (accumulator.has(key)) {
+            accumulator.get(key).push(currentValue);
         } else {
             // 否则，在 accumulator 中创建新的 key-value 对
-            accumulator.set(currentValue.key, [currentValue]);
+            accumulator.set(key, [currentValue]);
         }
 
         return accumulator;
