7620be50a37672b692f58a673dc9964e250db11a
贾一饼 <lby.i@qq.com>
2025-01-01 23:29:05 +0800

    fix: time utils

--- src/utils/time/time.ts
+++ src/utils/time/time.ts
@@ -1,10 +1,14 @@
-import dayjs from "dayjs";
+import dayjs, {Dayjs} from "dayjs";
 
 
-export const toDate = (value:string) => dayjs(value, "YYYY-MM-DD HH:mm:ss.SSSZ");
+export const toDayjs = (value:string) => dayjs(value, "YYYY-MM-DD HH:mm:ss.SSSZ");
+export const dateToDayjs = (value:string) => dayjs(value, "YYYY-MM-DD");
 export const dateFmt = (value:string, format:string) => dayjs(value).format(format);
 export const dateFmtDate = (value:Date) => dayjs(value).format("YYYY-MM-DD");
 export const dateFmtDateTime = (value:Date) => dayjs(value).format("YYYY-MM-DD HH:mm:ss");
+
+export const dayjsDateFmtDate = (value:Dayjs) => value.format("YYYY-MM-DD");
+export const dayjsDateFmtDateTime = (value:Dayjs) => value.format("YYYY-MM-DD HH:mm:ss");
 
 export function timestamp(dateString:string) {
     return new Date(dateString).getTime()
