829d9eda8ed7500c3e563a52a1db9f9b5bcede7c
贾一饼 <lby.i@qq.com>
2025-01-08 23:09:59 +0800

    feat: time

--- src/utils/time/time.ts
+++ src/utils/time/time.ts
@@ -3,7 +3,8 @@ import dayjs, {Dayjs} from "dayjs";
 
 export const toDayjs = (value:string) => dayjs(value, "YYYY-MM-DD HH:mm:ss.SSSZ");
 export const dateToDayjs = (value:string) => dayjs(value, "YYYY-MM-DD");
-export const dateFmt = (value:string, format:string) => dayjs(value).format(format);
+export const dateTimeToDayjs = (value:string) => dayjs(value, "YYYY-MM-DD HH:mm:ss");
+export const dateFmt = (value:string, format:string) => dayjs(value,format);
 export const dateFmtDate = (value:Date) => dayjs(value).format("YYYY-MM-DD");
 export const dateFmtDateTime = (value:Date) => dayjs(value).format("YYYY-MM-DD HH:mm:ss");
 
