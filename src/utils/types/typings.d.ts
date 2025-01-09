aee43765325de9fbbf715dde939e45c5b47403da
贾一饼 <lby.i@qq.com>
2025-01-09 23:17:51 +0800

    fix: uni request

--- src/utils/types/typings.d.ts
+++ src/utils/types/typings.d.ts
@@ -1,1 +1,2 @@
+import {HttpResponse} from './response'
 export type Fetch<T> = (url: string, method: string, headers: Record<string, string>, body: any) => Promise<HttpResponse<T>>
--- src/utils/uniapp/unirequest.ts
+++ src/utils/uniapp/unirequest.ts
@@ -1,6 +1,4 @@
 import qs from 'qs'
-import type {ResData} from "@/utils/types";
-
 
 /* eslint-disable no-param-reassign */
 export type UniRequestOptions = Omit<UniApp.RequestOptions, 'url'> & {
@@ -12,11 +10,15 @@ export type UniRequestOptions = Omit<UniApp.RequestOptions, 'url'> & {
 
 type UniRequestInterceptor = (options: UniApp.RequestOptions) => UniApp.RequestOptions
 type UniResponseInterceptor = (
-    response: UniApp.RequestSuccessCallbackResult,
-) => UniApp.RequestSuccessCallbackResult
+    response: RequestSuccessCallbackResult,
+) => RequestSuccessCallbackResult|null
 type UniResponseErrorInterceptor = (
     err: UniApp.GeneralCallbackResult,
-) => UniApp.GeneralCallbackResult
+) => any
+
+export interface RequestSuccessCallbackResult extends  UniApp.RequestSuccessCallbackResult{
+    config?: UniRequestOptions
+}
 
 class UniRequest {
     constructor(defaultConfig?: UniRequestOptions) {
@@ -59,14 +61,12 @@ class UniRequest {
     // 发起请求，默认配置是defaultConfig，也可以传入config参数覆盖掉默认配置中某些属性
     public request<
         T extends string | AnyObject | ArrayBuffer = any,
-        D extends string | AnyObject | ArrayBuffer | undefined = any,
     >(
         method: 'GET' | 'POST' | 'PUT' | 'DELETE',
         url: string,
-        data?: D,
         config?: UniRequestOptions,
-    ): Promise<ResData<T>> {
-        return new Promise<ResData<T>>((resolve, reject) => {
+    ): Promise<T> {
+        return new Promise<T>((resolve, reject) => {
             // 接口请求支持通过 query 参数配置 queryString
             if (config?.query) {
                 const queryStr = qs.stringify(config.query)
@@ -87,44 +87,25 @@ class UniRequest {
                 url,
                 header,
                 timeout,
-                data,
                 success: (res) => {
+                     let resc:RequestSuccessCallbackResult ={ ...res, config}
                     // 执行响应拦截
                     for (const ri of this.responseInterceptors) {
-                        res = ri(res)
+                        if (!ri(resc)) {
+                            reject(resc)
+                            return
+                        }
                     }
-                    // 状态码 2xx，参考 axios 的设计
-                    if (
-                        res.statusCode >= 200 &&
-                        res.statusCode < 300 &&
-                        (res.data as ResData<T>).code === 0
-                    ) {
-                        // 2.1 提取核心数据 res.data
-                        resolve(res.data as ResData<T>)
-                    } else {
-                        // 其他错误 -> 根据后端错误信息轻提示
-                        !config?.hideErrorToast &&
-                        uni.showToast({
-                            icon: 'none',
-                            title: (res.data as ResData<T>).msg || '请求错误',
-                        })
-                        // if (res.statusCode === 401) {
-                        //   // 401错误  -> 清理用户信息，跳转到登录页
-                        //   userStore.clearUserInfo()
-                        //   uni.navigateTo({ url: '/pages/user/login' })
-                        // }
-                        reject(res)
-                    }
+                    resolve(resc.data as T)
                 },
                 fail: (err) => {
                     // 执行响应错误拦截
                     for (const ei of this.responseErrorInterceptors) {
-                        err = ei(err)
+                       if(!ei(err)) {
+                           reject(err)
+                           return
+                       }
                     }
-                    uni.showToast({
-                        icon: 'none',
-                        title: '网络错误，换个网络试试',
-                    })
                     reject(err)
                 },
             }
@@ -140,18 +121,28 @@ class UniRequest {
 
     // 发起get请求
     public get<
-        T extends string | AnyObject | ArrayBuffer = any,
-        D extends string | AnyObject | ArrayBuffer | undefined = any,
-    >(url: string, params?: D, config?: UniRequestOptions) {
-        return this.request<T, D>('GET', url, params, config)
+        T extends string | AnyObject | ArrayBuffer = any
+    >(url: string, config?: UniRequestOptions) {
+        return this.request<T>('GET', url, config)
     }
 
     // 发起post请求
     public post<
-        T extends string | AnyObject | ArrayBuffer = any,
-        D extends string | AnyObject | ArrayBuffer | undefined = any,
-    >(url: string, data?: D, config?: UniRequestOptions) {
-        return this.request<T, D>('POST', url, data, config)
+        T extends string | AnyObject | ArrayBuffer = any
+    >(url: string,  config?: UniRequestOptions) {
+        return this.request<T>('POST', url,  config)
+    }
+
+    public put<
+        T extends string | AnyObject | ArrayBuffer = any
+    >(url: string,  config?: UniRequestOptions) {
+        return this.request<T>('PUT', url,  config)
+    }
+
+    public delete<
+        T extends string | AnyObject | ArrayBuffer = any
+    >(url: string,  config?: UniRequestOptions) {
+        return this.request<T>('DELETE', url, config)
     }
 }
 
