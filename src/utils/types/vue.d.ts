dbd7501ce27d0945fdfb303359c1f1fa1cff68e4
贾一饼 <lby.i@qq.com>
2025-02-19 14:18:41 +0800

    fix: types

--- src/utils/types/vue.d.ts
+++ src/utils/types/vue.d.ts
@@ -1,82 +1,33 @@
-// 此文件跟同级目录的 global.d.ts 文件一样也是全局类型声明，只不过这里存放一些零散的全局类型，无需引入直接在 .vue 、.ts 、.tsx 文件使用即可获得类型提示
+import Vue, { VNode } from "vue";
 
-type RefType<T> = T | null;
-
-type EmitType = (event: string, ...args: any[]) => void;
-
-type TargetContext = "_self" | "_blank";
-
-type ComponentRef<T extends HTMLElement = HTMLDivElement> =
-  ComponentElRef<T> | null;
-
-type ElRef<T extends HTMLElement = HTMLDivElement> = Nullable<T>;
-
-type ForDataType<T> = {
-  [P in T]?: ForDataType<T[P]>;
-};
-
-type AnyFunction<T> = (...args: any[]) => T;
-
-type PropType<T> = VuePropType<T>;
-
-type Writable<T> = {
-  -readonly [P in keyof T]: T[P];
-};
-
-type Nullable<T> = T | null;
-
-type NonNullable<T> = T extends null | undefined ? never : T;
-
-type Recordable<T = any> = Record<string, T>;
-
-type ReadonlyRecordable<T = any> = {
-  readonly [key: string]: T;
-};
-
-type Indexable<T = any> = {
-  [key: string]: T;
-};
-
-type DeepPartial<T> = {
-  [P in keyof T]?: DeepPartial<T[P]>;
-};
-
-type Without<T, U> = { [P in Exclude<keyof T, keyof U>]?: never };
-
-type Exclusive<T, U> = (Without<T, U> & U) | (Without<U, T> & T);
-
-type TimeoutHandle = ReturnType<typeof setTimeout>;
-
-type IntervalHandle = ReturnType<typeof setInterval>;
-
-type Effect = "light" | "dark";
-
-interface ChangeEvent extends Event {
-  target: HTMLInputElement;
+declare module "*.tsx" {
+  import Vue from "compatible-vue";
+  export default Vue;
 }
 
-interface WheelEvent {
-  path?: EventTarget[];
+declare global {
+  namespace JSX {
+    interface Element extends VNode {}
+    interface ElementClass extends Vue {}
+    interface ElementAttributesProperty {
+      $props: any;
+    }
+    interface IntrinsicElements {
+      [elem: string]: any;
+    }
+    interface IntrinsicAttributes {
+      [elem: string]: any;
+    }
+  }
 }
 
-interface ImportMetaEnv extends ViteEnv {
-  __: unknown;
+declare module "*.vue" {
+  import type { DefineComponent } from "vue";
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
 }
 
-interface Fn<T = any, R = T> {
-  (...arg: T[]): R;
+declare module "*.scss" {
+  const scss: Record<string, string>;
+  export default scss;
 }
-
-interface PromiseFn<T = any, R = T> {
-  (...arg: T[]): Promise<R>;
-}
-
-interface ComponentElRef<T extends HTMLElement = HTMLDivElement> {
-  $el: T;
-}
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-function parseInt(s: string | number, radix?: number): number;
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-function parseFloat(string: string | number): number;
