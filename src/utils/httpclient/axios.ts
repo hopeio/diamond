eb06959530278c41bdeb07ce190351180917fc2b
liov-ola <liov@olaparty.sg>
2025-12-10 19:39:42 +0800

    feat: httpclient

---
+++ src/utils/httpclient/axios.ts
@@ -0,0 +1,125 @@
+import axios, {
+    type AxiosInstance,
+    type AxiosError,
+    type AxiosResponse,
+    type Method,
+    type HeadersDefaults,
+    type AxiosInterceptorOptions, type InternalAxiosRequestConfig, type AxiosRequestConfig, type AxiosRequestHeaders,
+    type AxiosHeaderValue,
+} from "axios";
+
+
+interface Error extends AxiosError {
+    isCancelRequest?: boolean;
+}
+
+interface Response extends AxiosResponse {
+    config: InternalRequestConfig;
+}
+
+interface InternalRequestConfig extends RequestConfig {
+    headers: AxiosRequestHeaders;
+}
+
+interface RequestConfig extends AxiosRequestConfig {
+    successMsg?: string;
+    beforeRequestCallback?: (request: RequestConfig) => void;
+    beforeResponseCallback?: (response: Response) => void;
+}
+
+interface Defaults extends Omit<RequestConfig, 'headers'> {
+    headers: HeadersDefaults & {
+        [key: string]: AxiosHeaderValue
+    }
+}
+
+type RequestInterceptorUse<T> = (
+    onFulfilled?: ((value: T) => T | Promise<T>) | null,
+    onRejected?: ((error: any) => any) | null,
+    options?: AxiosInterceptorOptions
+) => number;
+
+type ResponseInterceptorUse<T> = (
+    onFulfilled?: ((value: T) => T | Promise<T>) | null,
+    onRejected?: ((error: any) => any) | null
+) => number;
+
+interface InterceptorManager<V> {
+    use: V extends Response
+        ? ResponseInterceptorUse<V>
+        : RequestInterceptorUse<V>;
+
+    eject(id: number): void;
+
+    clear(): void;
+}
+
+export class HttpClient {
+    constructor(config?: RequestConfig) {
+        if (config) {
+            this.instance = axios.create(config);
+            this.defaults = this.instance.defaults;
+            this.interceptors = {
+                request: this.instance.interceptors.request,
+                response: this.instance.interceptors.response
+            };
+        }
+    }
+
+    /** 保存当前`Axios`实例对象 */
+    defaults: Defaults = axios.defaults;
+    instance: AxiosInstance = axios;
+
+    interceptors: {
+        request: InterceptorManager<InternalAxiosRequestConfig>;
+        response: InterceptorManager<Response>;
+    } = {
+        request: axios.interceptors.request,
+        response: axios.interceptors.response
+    }
+
+    /** 通用请求工具函数 */
+    public request<T>(
+        method: Method,
+        url: string,
+        config?: RequestConfig
+    ): Promise<T> {
+        if (!config) {
+            config = {method: method, url: url};
+        } else {
+            config.method = method;
+            config.url = url;
+        }
+
+        // 单独处理自定义请求/响应回调
+        return new Promise((resolve, reject) => {
+            this.instance
+                .request(config)
+                .then(response => {
+                    resolve(response.data);
+                }).catch((err: Error) => {
+                    reject(err);
+                });
+        });
+    }
+
+    /** 单独抽离的`post`工具函数 */
+    public post<T>(url: string, params?: RequestConfig): Promise<T> {
+        return this.request<T>("post", url, params);
+    }
+
+    /** 单独抽离的`get`工具函数 */
+    public get<T>(url: string, params?: RequestConfig): Promise<T> {
+        return this.request<T>("get", url, params);
+    }
+
+    public put<T>(url: string, params?: RequestConfig): Promise<T> {
+        return this.request<T>("put", url, params);
+    }
+
+    public delete<T>(url: string, params?: RequestConfig): Promise<T> {
+        return this.request<T>("delete", url, params);
+    }
+}
+
+export const httpClient = new HttpClient();
---
+++ src/utils/httpclient/type.ts
@@ -0,0 +1,9 @@
+
+export type ResponseType =
+    | 'arraybuffer'
+    | 'blob'
+    | 'bytes'
+    | 'json'
+    | 'text'
+    | 'stream'
+    | 'formdata';
